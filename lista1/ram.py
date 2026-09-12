fbb8eeb93977e92270672fc0a51ebd1fd83afded
Raphael Ramos <raphael.opensource@gmail.com>
2026-09-12 11:37:51 -0300

    fix RAM operation tracking on insertion sort

--- lista1/ram.py
+++ lista1/ram.py
@@ -1,34 +1,57 @@
 def insertion_sort_ram(lista):
-    # NOTE: cada linha tem custo unitario, contado a cada execucao (c1 = c2 = ... = 1)
-    # NOTE: estrutura "for" substituido pelo "while" para ter maior
-    # controle nas operacoes de verificacao de indice
+    # ignoramos as operacoes das inicializacoes
     n = len(lista)
+    indices_lista = range(1, n)
     operacoes = 0
 
-    j = 1
-    while True:
-        operacoes += 1
-        if j >= n:
-            break
+    # operacoes contam a partir daqui
+    for i in indices_lista:
+        # 1 op: acessar posicao de memoria i em indices_lista
+        # 1 op: calcular i + 1
+        # 1 op: atribuir i
+        operacoes += 3
 
-        chave = lista[j]
-        operacoes += 1
+        chave = lista[i]
+        # 1 op: acessar posicao de memoria i em lista
+        # 1 op: atribuir chave
+        operacoes += 2
 
-        i = j - 1
-        operacoes += 1
+        j = i - 1
+        # 1 op: calcular i - 1
+        # 1 op: atribuir j
+        operacoes += 2
 
-        while True:
-            operacoes += 1
-            if not (i >= 0 and lista[i] > chave):
-                break
-            lista[i + 1] = lista[i]
-            operacoes += 1
-            i -= 1
-            operacoes += 1
+        while j >= 0 and lista[j] > chave:
+            # 1 op: calcular j >= 0
+            # 1 op: acessar posicao de memoria j em lista
+            # 1 op: ler chave
+            # 1 op: calcular lista[j] > chave
+            # 1 op: calcular valor lógico do AND
+            operacoes += 5
 
-        lista[i + 1] = chave
-        operacoes += 1
+            lista[j + 1] = lista[j]
+            # 1 op: calcula 1 + 1
+            # 1 op: acessa posicao de memoria j em lista
+            # 1 op: acessa posicao de memoria j + 1 em lista
+            # 1 op: atribuir lista[j] em lista[j + 1]
+            operacoes += 4
 
-        j += 1
+            j -= 1
+            # 1 op: calcular j - 1
+            # 2 op: atribuir j
+            operacoes += 2
+
+        # 5 op: avaliacao de saida do while
+        operacoes += 5
+
+        # 1 op: calcular j + 1
+        # 1 op: acessa posicao de memoria j + 1 em lista
+        # 1 op: ler chave
+        # 1 op: atribuir chave em lista[j+1]
+        lista[j + 1] = chave
+        operacoes += 4
+
+    # 3 op: avaliacao de saida do for
+    operacoes += 3
 
     return lista, operacoes
